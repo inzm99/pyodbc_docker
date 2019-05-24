9ef6b3af04817ac5d798ac06a9e8d5fe8b3ae251
inzm99 <41764720+inzm99@users.noreply.github.com>
2019-05-24 09:37:25 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,17 +4,11 @@ server = 'XXXXXX'
 username = 'XXXXXXX'
 password = 'XXXXXX'
 port ='1433'
+tsql ="select @@version"
 
-try:
-    cnn = pyodbc.connect('DRIVER={FreeTDS};SERVER='+server+';PORT='+port+';UID='+username+';PWD='+ password)
-    cur = cnn.cursor()
-    type_no = 7
-    cur.execute("""select @@version""")
-    rows = cur.fetchall()
-    print(rows)
-
-    cur.close()
-    cnn.close()
-except (pyodbc.Error) as e:
-    print (e)
-    print (e.args[1])
+cnn = pyodbc.connect('DRIVER={FreeTDS};SERVER='+server+';PORT='+port+';UID='+username+';PWD='+ password)
+cur = cnn.cursor()
+with
+    cur.execute(tsql)
+    row = cur.fetchone()
+    print(str(row[0]))
